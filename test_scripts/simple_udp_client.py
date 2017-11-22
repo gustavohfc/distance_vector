2881e9b0fca1896be53fd3d6bf48998cf2bff0aa
Gustavo Henrique Fernandes Carvalho <gustavohfcarvalho@gmail.com>
2017-11-22 00:36:21 -0200

    Simple UDP server and client in python.

---
+++ test_scripts/simple_udp_client.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import socket
+
+server_address = '127.0.0.1'
+server_port = 12345
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+server = (server_address, server_port)
+
+while True:
+	msg = raw_input()
+	sock.sendto(msg, server)
---
+++ test_scripts/simple_udp_server.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import socket
+
+server_address = '127.0.0.1'
+server_port = 12345
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+server = (server_address, server_port)
+sock.bind(server)
+print("Listening on " + server_address + ":" + str(server_port))
+
+while True:
+	msg, client_address = sock.recvfrom(1024)
+	print(str(client_address) + ": " + msg)
